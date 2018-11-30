4e8c16ecf479c9b8124d43977d0220d6dfb1462e
Isabella Oliveira <isabellaro@al.insper.edu.br>
2018-11-30 20:14:12 -0200

    Adicionando o install

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install python3.6 -y
+sudo apt install python3-pip -y
+sudo apt-get install git-core -y
+sudo pip3 install flask_restful
+sudo pip3 install flask_httpauth
+cd CloudInsper/
+cd webapp
+python3 app.py
